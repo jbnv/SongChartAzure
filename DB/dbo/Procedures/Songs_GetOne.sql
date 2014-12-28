3cd8bc9f460f5f2f6a87c7c82680d3a5f0479b57
Jay Bienvenu <github@bienv.com>
2014-12-27 21:05:27 -0600

    Song detail view and SP.

---
+++ DB/dbo/Procedures/Songs_GetOne.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[Songs_GetOne]
+	@fullname VARCHAR(10)
+AS
+SELECT		[Fullname],
+			[Title],
+			[ArtistFullname],
+			[ArtistTitle],
+			[GenreFullname],
+			[GenreTitle],
+			[DebutDateFullname],
+			[Ranks],
+			[Tags],
+			[Score] 
+FROM		[dbo].[Songs_Detailed]
+WHERE		[Fullname] = @fullname
+RETURN 0
---
+++ DB/dbo/Views/Songs_Detailed.sql
@@ -0,0 +1,15 @@
+﻿CREATE VIEW [dbo].[Songs_Detailed]
+AS 
+SELECT		s.[Fullname],
+			s.[Title],
+			s.[ArtistFullname],
+			a.[Title] AS [ArtistTitle],
+			s.[GenreFullname],
+			g.[Title] AS [GenreTitle],
+			[DebutDateFullname],
+			[Ranks],
+			[Tags],
+			s.[Score] 
+FROM		[Songs] s
+LEFT OUTER JOIN	[Artists] a ON a.Fullname = s.ArtistFullname
+LEFT OUTER JOIN [Genres] g ON g.Fullname = s.GenreFullname
