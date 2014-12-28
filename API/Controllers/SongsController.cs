d8f8de9809893387204e738cb7a24c54a227c73d
Jay Bienvenu <github@bienv.com>
2014-12-28 10:53:31 -0600

    API bare-bones implementation.

---
+++ API/Controllers/SongsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using API.Models;
+using API.Tools;
+
+namespace API.Controllers
+{
+    public class SongsController : ApiController
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        public Song Get([FromUri] string fullname)
+        {
+            //if (fullname.Substring(0, 2) != "s:")
+            //{
+            //    fullname = "s:" + fullname;
+            //}
+
+            //Song song;
+            //GetAction<Song> getAction = new GetAction<Song>();
+            //getAction.AddParameter(new SqlParameter("@Fullname", fullname));
+            //getAction.ProcedureName = "Songs_GetOne";
+            //getAction.IDataRecordFunc = Song.FromDataRecord;
+            //song = getAction.Execute()[0];
+
+            ////if (song == null)
+            ////{
+            ////    return NotFound();
+            ////}
+            ////return Ok(song);
+            //return song;
+            return new Song();
+        }
+    
+
+        //public IEnumerable<Song> GetAllSongs()
+        //{
+        //    //TODO Call the appropriate procedure in database.
+        //}
+
+    }
+}
---
+++ API/Models/Song.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+
+namespace API.Models
+{
+    public class Song
+    {
+        public string Fullname { get; set; }
+        public string Title { get; set; }
+        public Artist Artist { get; set; }
+        public Genre Genre { get; set; }
+        public Decimal Score { get; set; }
+        public Decimal DebutRank { get; set; }
+        public Decimal PeakRank { get; set; }
+        public Decimal Duration { get; set; }
+        //TODO public DebutDate { get; set; }
+        public Decimal[] Ranks { get; set;  }
+
+        public Song()
+        {
+
+        }
+
+        public static  Song FromDataRecord(IDataRecord dr)
+        {
+            Song o = new Song();
+            int ordinal;
+
+            o.Fullname = dr.GetString(dr.GetOrdinal("Fullname"));
+
+            o.Title = dr.GetString(dr.GetOrdinal("Title"));
+
+            //public Artist Artist { get; set; }
+            //public Genre Genre { get; set; }
+            //public Decimal[] Ranks { get; set;  }
+
+            ordinal = dr.GetOrdinal("Score");
+            o.Score = dr.IsDBNull(ordinal) ? 0 : dr.GetDecimal(ordinal);
+
+            ordinal = dr.GetOrdinal("DebutRank");
+            o.DebutRank = dr.IsDBNull(ordinal) ? 0 : dr.GetDecimal(ordinal);
+
+            ordinal = dr.GetOrdinal("PeakRank");
+            o.PeakRank = dr.IsDBNull(ordinal) ? 0 : dr.GetDecimal(ordinal);
+
+            ordinal = dr.GetOrdinal("Duration");
+            o.Duration = dr.IsDBNull(ordinal) ? 0 : dr.GetDecimal(ordinal);
+
+            return o;
+        }
+
+    }
+}
