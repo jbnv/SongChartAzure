85fcd15a1cb0b1870560251d1aed43b5cb6fac07
Jay Bienvenu <github@bienv.com>
2015-01-14 19:55:46 -0600

    Navigation fix and cosmetic changes.

--- UI/app/viewmodels/shell.js
+++ UI/app/viewmodels/shell.js
@@ -8,15 +8,19 @@
             term = ''+searchTerm();
             console.log('searchTerm', term);
             if (result = term.match(/^(\d\d\d\d)s$/)) {
-                router.navigate('decade/'+result[1]);
+                router.navigate('decade/' + result[1]);
+                return;
             }
             if (result = term.match(/^(\d\d\d\d)$/)) {
                 router.navigate('year/' + result[1]);
+                return;
             }
             if (result = term.match(/^(\d\d\d\d)-(\d\d)$/)) {
                 router.navigate('month/' + result[1] + '/' + result[2]);
+                return;
             }
-            app.showMessage('Search for "' + term + '" not yet implemented.');        },
+            app.showMessage('Search for "' + term + '" not yet implemented.');
+        },
         activate: function () {
             router.map([
                 { route: '', title:'Home Screen', moduleId: 'viewmodels/home' },
@@ -26,9 +30,11 @@
                 { route: 'song/:id', title: 'Song Details', moduleId: 'viewmodels/artist' },
                 { route: 'artist/:id', title: 'Artist Details', moduleId: 'viewmodels/artist' },
                 { route: 'genre/:id', title: 'Genre Details', moduleId: 'viewmodels/genre' },
-                { route: 'topByPeak', title: 'Genre Details', moduleId: 'viewmodels/songChart' },
-                { route: 'topByDebut', title: 'Genre Details', moduleId: 'viewmodels/songChart' },
-                { route: 'topByDuration', title: 'Genre Details', moduleId: 'viewmodels/songChart' }
+                //TODO { route: 'artists', title: 'Artists', moduleId: 'viewmodels/artistChart', nav: true },
+                //TODO { route: 'genres', title: 'Genres', moduleId: 'viewmodels/genreChart', nav: true },
+                { route: 'topByPeak', title: 'Top by Peak Rank', moduleId: 'viewmodels/songChart', nav: true },
+                { route: 'topByDebut', title: 'Top by Debut Rank', moduleId: 'viewmodels/songChart', nav: true },
+                { route: 'topByDuration', title: 'Top by Longevity', moduleId: 'viewmodels/songChart', nav: true }
             ]).buildNavigationModel();
             
             return router.activate();
