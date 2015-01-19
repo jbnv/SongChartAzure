9a71a853da108e97063b7b8845209a732e0fdc3f
Jay Bienvenu <github@bienv.com>
2015-01-18 21:30:21 -0600

    SongTitleWords_Update: Implement massaging.

--- DB/dbo/Procedures/SongTitleWords_Update.sql
+++ DB/dbo/Procedures/SongTitleWords_Update.sql
@@ -3,11 +3,30 @@ AS
 	TRUNCATE TABLE [SongTitleWords];
 
 	INSERT INTO [dbo].[SongTitleWords] ([SongId],[Word])
-	SELECT DISTINCT [Id],LOWER(words.Item)
+	SELECT DISTINCT [Id],COALESCE(reduc.[Word],s3.[Word])
 	FROM [Songs]
-	--TODO Massage title. Map related words together.
-	CROSS APPLY (SELECT REPLACE(REPLACE(REPLACE([Title],' ','_'),'(',''),')','') AS [TitleM]) massage
-	CROSS APPLY [dbo].[Split]([TitleM],'_') words
+	CROSS APPLY (
+		SELECT LOWER(REPLACE([Songs].[Title],' ','-')) AS [String] 
+	) s1
+	CROSS APPLY (
+		SELECT (
+			SELECT	SUBSTRING(s1.[String], N, 1)
+			FROM	dbo.Numbers
+			WHERE	N > 0 AND N <= CONVERT(INT, LEN(s1.[String]))
+				AND SUBSTRING(s1.[String], N, 1) NOT IN ('(',')',',','.')
+			FOR XML PATH('')
+		) [String]
+	) s2
+	CROSS APPLY (
+		SELECT	SUBSTRING(s2.[String], N, CHARINDEX('-', s2.[String] + '-', N) - N) AS [Word]
+		FROM	dbo.Numbers
+		WHERE	N <= CONVERT(INT, LEN(s2.[String]))
+			AND SUBSTRING('-' + s2.[String], N, 1) = '-'	
+	) s3
+	LEFT OUTER JOIN [dbo].[ReductionWords] reduc  ON reduc.[Word] = s3.[Word]
+	WHERE s3.[Word] NOT IN (
+		'&amp;','a','an','and','are','as','for','in','is','it','its','of','on','the','then','these','those','was','were','with','would'
+	)
 	ORDER BY [Id]
 
 RETURN 0
